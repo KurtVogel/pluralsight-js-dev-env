08b081783b1d763c342bf6ec0b4611c7239eed47
Vesa Tamminen <vestuu@gmail.com>
2017-11-03 08:17:45 +0200

    API ja HTTP mockausta ja kutsumista

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './index.css';
 
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 // Populate table of users via API call.
 getUsers().then(result => {
-  let usersBody = "";
 
+  let usersBody = "";
   result.forEach(user => {
     usersBody+= `<tr>
       <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
@@ -17,4 +17,18 @@ getUsers().then(result => {
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array from a DOM collection
+  // getELementsByClassname only return an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function (event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 });
